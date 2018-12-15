fdca9526b9dc35fef4f9fe9765b22e3a1e4187c3
Astanislav1990 <stanislav.eduardovich@gmail.com>
2018-12-15 18:17:31 +0300

    All my files

---
+++ Tasks/Task02/Binary.java
@@ -0,0 +1,32 @@
+class Binary {
+  public static void main (String[] args)  {
+    int number = 137;
+    int a = 2;
+    int b = number / a;
+    int c = number % a;
+    int d = b / a;
+    int e = b % a;
+    int f = d / a;
+    int g = d % a;
+    int x = f / a;
+    int y = f % a;
+    int z = x / a;
+    int w = x % a;
+    int m = z / a;
+    int n = z % a;
+    int t = m / a;
+    int t1 = m % a;
+    int p = t / a;
+    int p1 = t % a;
+    /*System.out.println(c);  
+    System.out.println(e);
+    System.out.println(g);
+    System.out.println(y);
+    System.out.println(w);
+    System.out.println(n);
+    System.out.println(t1);
+    System.out.println(p1);*/
+    System.out.println(p1 + " " + t1 + " " + n + " " + w + " " + y + " " + g + " " + e + " " + c + " ");
+    
+    }
+  }
