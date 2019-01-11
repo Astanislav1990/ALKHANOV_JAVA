0efc90b5718da7eaae6d253fe9b2180e0f56a27f
Astanislav1990 <stanislav.eduardovich@gmail.com>
2019-01-11 20:28:10 +0300

    modified Task 09

--- Tasks/Task09/ConvertCharArraytoNum.java
+++ Tasks/Task09/ConvertCharArraytoNum.java
@@ -6,10 +6,12 @@ public class ConvertCharArraytoNum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         char a[] = scanner.nextLine().toCharArray();
+        int number = 0;
         for (int i = 0; i < a.length; i++) {
              int  x =(int) a[i] - '0';
-               System.out.print (x);
+             number = number * 10 + x;
         }
+        System.out.print (number);
     }
 }
 
